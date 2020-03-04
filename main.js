7c142ff1ff511a21c951d54d5f7f6dd3b47fed1a
Joseph <joejoshw@gmail.com>
2020-03-04 17:25:10 +0300

    Adde search, add and remove functions and basic tests

--- main.js
+++ main.js
@@ -1,26 +1,44 @@
 class LWW_Element_Set {
-    constructor() {
-        this._setA = new Map();
-        this._setR = new Map();
+    constructor({
+        bias = 1,
+        addSet = new Map(), // _addSet is the array containing all added elements
+        removeSet = new Map() // _removeSet is the array containing all removed elements
+    } = {} ) {
+        this.biasToAdd = biasToAdd;
+        this._addSet = addSet;
+        this._removeSet = removeSet;
     }
 
-    add(data, time) {
-        const oldA = this._setA.get(data);
-        if (checkIfThisIsTheLastEntry(oldA, time)) {
-            this._setA.set(data, time);
+    //Add a new entry to the added elements
+    add(elem, time = +new Date()) {
+        let { data } = elem;
+        if (!this._addSet.has(data) || this._addSet.get(data).time < time) {
+            this._addSet.set(data, { elem, time });
+        }
+
+        return this;
+    }
+
+    //Add a new entry to the removed elements
+    remove(elem, time = +new Date) {
+        let { data } = elem;
+        if (this.search(elem) && ( !this._addSet.get(data).time < time)) {
+            this._removeSet.set(data, {elem, time});
         }
     }
 
-    remove(data, time) {
-        const oldR = this._setR.get(data);
-        if (checkIfThisIsTheLastEntry(oldR, time)) {
-            this._setR.set(data, time);
-        }
+    // Check whether an element is in the set
+    search({data}) {
+        return this._addSet.has(data) && (
+            !this._removeSet.has(data) ?
+                true : 
+                (
+                    this.biasToAdd ?
+                    this._addSet.get(data).time >= this._removeSet.get(data).time :
+                    this._addSet.get(data).time > this._removeSet.get(data).time
+                )
+        );
     }
-}
-
-function checkIfThisIsTheLastEntry(oldTime, newTime) {
-    return !oldTime || oldTime < newTime;
 }
 
 module.exports = LWW_Element_Set;
--- test.js
+++ test.js
@@ -1,12 +1,34 @@
-let lwwwSet = require('./main');
+let LWW_Element_Set = require('./main');
 
-describe("LWWElementSet", () => {
-    it("adds latest data", () => {
-        const lwwSet = new lwwwSet();
-        lwwSet.add("set1", 1);
-        lwwSet.add("set2", 1);
-        lwwSet.add("set3", 3);
-        const expected = new Map([["set3", 3], ["set1", 1]]);
-        excpect(lwwSet._setA).toEqual(expected);
-    })
-})
+
+// Test wheter the search function works
+(() => {
+    let addSet = new Map([ ['test1', {time: 1}], ['test2', {time: 2}]]);
+    let testSet = new LWW_Element_Set({addSet});
+    console.log("Testing whether search function works");
+    // We add test1 and tes2 to the set
+    // test1 and test2 should be present
+    // test3 should not be present
+    console.assert(testSet.search({data: 'test1'}), 'Expected True: test1 should be present');
+    console.assert(testSet.search({data: 'test2'}), 'Expected True: test2 should be present');
+    console.assert(!testSet.search({data: 'test3'}), 'Expected False: test3 should not be present');
+})();
+
+// Test whether add/remove works
+(() => {
+    let testSet = new LWW_Element_Set();
+    console.log("Testing whether add/remove functions work");
+
+    testSet.remove( {data: 'test1'}, 2 );
+    console.assert( !testSet._removeSet.has('test1'), 'Expected false: test1 should not be present.');
+    console.assert( !testSet.search({ data: 'test1'}), 'Expected false: test1 should not be present. It has not been added to _removeSet not _addSet');
+
+    testSet.add( {data: 'test1'}, 3);
+
+    testSet.remove( {data: 'test1'}, 2);
+
+    testSet.remove( {data: 'test1'}, 4);
+
+    testSet.add( {data: 'test1'}, 5);
+
+})();
